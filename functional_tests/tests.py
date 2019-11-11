c853e25e2d707d774a5cf4cb3fb4e77e6b232894
Bharat Shrestha <bharatshrestha925@gmail.com>
2019-11-11 15:31:17 +0545

    functional_test changed to liveservertestcase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,13 @@
 import time
 import unittest
 
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -16,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 	def check_for_row_in_list_table(self,row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_element_by_tag_name('tr')
-		self.assertIn(row_text, rows.text)
+		self.assertIn(row_text, [row.text for row in rows])
 
 
 
@@ -36,14 +38,14 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		self.check_for_row_in_list_table('Buy Peacock feathers')
+		self.check_for_row_in_list_table('1:Buy Peacock feathers')
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		self.check_for_row_in_list_table('Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('Finish the test!')
 
